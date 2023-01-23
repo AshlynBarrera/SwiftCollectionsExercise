fb0faf0abd1d3d9982472cf7ec99552991443eff
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-01-23 09:06:24 -0600

    Initial Commit

---
+++ swiftCollectionsExercise.playground/Contents.swift
@@ -0,0 +1,102 @@
+
+
+
+
+
+//1a
+var nums = [2, 64, 1]
+
+//1b
+print(nums)
+
+//1c
+print(nums.count)
+
+//1e
+print(nums[2])
+
+//1g
+print(nums.sorted())
+
+//1f
+print(nums.reversed())
+
+//1g
+print(nums.shuffled())
+
+//2a
+var marioBros = [""]
+
+//2b
+marioBros.append("Mario")
+
+//2c
+marioBros.append("Luigi")
+
+//2e
+print(marioBros[2])
+
+//2
+marioBros.insert("Toad", at: 2)
+//2
+marioBros.insert("Princess Peach", at: 0);
+print(marioBros)
+
+//3a
+var marioVillains = [""]
+
+//3b
+marioVillains.insert("Bowser", at: 0);
+print(marioVillains)
+
+//3c
+marioVillains.insert("Koopa Troopa", at: 1);
+
+//3d
+print(marioVillains)
+
+//3e
+print(marioVillains.contains("Yosho"));
+
+//4a
+var marioBrosGames = [""];
+
+//4b
+marioBrosGames.append("Super Mario World");
+
+//4d
+var  marioBrosGamesSet: Set<String> = [""]
+print(marioBrosGamesSet)
+
+//5a
+let characters: [String : String] = [
+     "Mario" : "Chris Pratt",
+     "Luigi" : "Charlie Day",
+]
+
+//5b
+print(characters)
+
+
+//Bonus
+
+//
+var otherCharactersOne = [
+    ["Donkey Kong", "Cranky Kong"],
+    ["Wario", "King Boo"]
+]
+//
+
+//1a
+print(otherCharactersOne[0])
+
+//1b
+print(otherCharactersOne[1])
+
+
+
+
+
+
+
+
